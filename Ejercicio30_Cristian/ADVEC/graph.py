e2917051b1d98401c8dfd5703af8ed300432bf37
jaimetodos <45340056+jaimetodos@users.noreply.github.com>
2018-12-06 10:44:59 -0500

    Add files via upload

---
+++ Ejercicio30_Cristian/ADVEC/graph.py
@@ -0,0 +1,14 @@
+import matplotlib
+matplotlib.use("Agg")
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+data = np.loadtxt('advecParallel.dat')
+plt.plot(data[0], data[1], label='Parallel', alpha=0.5, lw=4,color = 'red')
+
+data = np.loadtxt('advecSerial.dat')
+plt.plot(data[0], data[1], label='Serial',color = 'black')
+
+plt.legend()
+plt.savefig('adveccion.pdf')
